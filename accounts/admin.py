2f60c9aa39af58dc611cc3aec11795169e300784
TKaburu <traceymwendwa@gmail.com>
2024-07-15 12:23:34 +0300

    feat: registered custom User to admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    """
+    This class defines a custom user on the admin page
+    and list its fieldsets
+    """
+    model = CustomUser
+    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
