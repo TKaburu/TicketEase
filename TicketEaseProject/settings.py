e414ca5e21efb4a83a724a1e8bcc0524b5fb9335
TKaburu <traceymwendwa@gmail.com>
2024-07-15 11:04:32 +0300

    feat: settings for authentication using django-allauth

--- TicketEaseProject/settings.py
+++ TicketEaseProject/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # local installs
+    'accounts.apps.AccountsConfig',
 
     # Third party installs
     'allauth',
@@ -46,7 +47,28 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
+
+#         <==================== Accounts/ authentication settings ====================>
+
+AUTH_USER_MODEL = 'accounts.CustomUser'  # Django to use my custom user rather than built in User
+
+LOGIN_URL = 'accounts_login'
+LOGIN_URL_REDIRECT = 'home'
+
+# allauth settings
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'  # user is redurected to login page after logging out
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'optional'
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_ADAPTER = 'accounts.adapters.TicketEaseAdapter'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',  # will use allauth for authentication
+)
 
 ROOT_URLCONF = 'TicketEaseProject.urls'
 
@@ -110,11 +132,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+#         <==================== static files settings ====================>
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#  media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
