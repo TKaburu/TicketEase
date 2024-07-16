863557cc27e0a07514fc9e21fb7fd0efaf3bf666
TKaburu <traceymwendwa@gmail.com>
2024-07-16 08:10:13 +0300

    feat: search functionality for tickets

--- tickets/views.py
+++ tickets/views.py
@@ -18,24 +18,36 @@ def ticketsView(request):
     """
     Logic for the tickets page
     """
-    status = request.GET.get('status')  # Get the status from the query parameters
+    tickets = Ticket.objects.all()
+    status = request.GET.get('status')  # to filter by status
+    query = request.GET.get('q')  # search parameter
     if status:
         tickets = Ticket.objects.filter(status=status)
+
+    elif query:
+        tickets = tickets.filter(
+            Q(title__icontains=query) |  # Case-insensitive search in title
+            Q(description__icontains=query) |
+            Q(status__icontains=query) |
+            Q(created_by__username__icontains=query) |
+            Q(assigned_to__username__icontains=query)
+        )
+
     else:
         tickets = Ticket.objects.all()
-    
     # Get the counts for each status
-    open_count = Ticket.objects.filter(status='open').count()
-    pending_count = Ticket.objects.filter(status='pending').count()
-    closed_count = Ticket.objects.filter(status='closed').count()
     all_count = Ticket.objects.count()
+    open_count = Ticket.objects.filter(status='open').count()
+    assigned_count = Ticket.objects.filter(status='assigned').count()
+    closed_count = Ticket.objects.filter(status='closed').count()
 
     context = {
         'tickets': tickets,
         'open_count': open_count,
-        'pending_count': pending_count,
+        'assigned_count': assigned_count,
         'closed_count': closed_count,
         'all_count': all_count,
         'selected_status': status,
     }
     return render(request, 'tickets/tickets-view.html', context)
+
