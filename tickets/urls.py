9a698858350ceaab5d0571c840de8fbb1f13560a
TKaburu <traceymwendwa@gmail.com>
2024-07-18 07:14:59 +0300

    fix: url pattern for create ticket

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', home, name='home'),
     path('tickets/', ticketsView, name='tickets-view'),
     path('tickets/<slug:slug>/', ticketDetails, name='ticket-details'),
+    path('create-ticket/', createTicket, name='create-ticket'),
     path('tickets/<slug:slug>/accept/', acceptTicket, name='accept-ticket'),
     path('delete-message/<int:msg_id>/', deleteMessage, name='delete-message'),
 ]
