5c4adc0369848fdfd3bc5dc2e9a90405e9941ce4
TKaburu <traceymwendwa@gmail.com>
2024-07-18 07:25:19 +0300

    feat: fom for creating a ticket

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from .models import *
 
+class NewTicketForm(forms.ModelForm):
+    class Meta:
+        model = Ticket
+        fields = ['title', 'description', 'attachments']
+
 class TicketMessageForm(forms.ModelForm):
     """
     This form handles logic for messaging in the detail ticket view
