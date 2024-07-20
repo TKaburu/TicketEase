20ac434cf73265752703e5084a097ffb31256a95
TKaburu <traceymwendwa@gmail.com>
2024-07-20 18:27:46 +0300

    fix: only clients can create tickets

--- tickets/urls.py
+++ tickets/urls.py
@@ -5,9 +5,12 @@ urlpatterns = [
     path('', home, name='home'),
     path('tickets/', ticketsView, name='tickets-view'),
     path('tickets/<slug:slug>/', ticketDetails, name='ticket-details'),
+
     path('create-ticket/', createTicket, name='create-ticket'),
+
     path('tickets/<slug:slug>/accept/', acceptTicket, name='accept-ticket'),
     path('tickets/<slug:slug>/close/', closeTicket, name='close-ticket'),
+
     path('engineer/<str:username>/dashboard', engineerDashboard, name='engineer-dashboard'),
     path('client/<str:username>/dashboard', clientDashboard, name='client-dashboard'),
     path('delete-message/<int:msg_id>/', deleteMessage, name='delete-message'),
